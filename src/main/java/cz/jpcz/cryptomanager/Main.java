3d2806edc34a3bc76184de95220a0c6ae73c06f8
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-27 22:01:49 +0200

    Repo init

---
+++ src/main/java/cz/jpcz/cryptomanager/Main.java
@@ -0,0 +1,14 @@
+package cz.jpcz.cryptomanager;
+
+import cz.jpcz.cryptomanager.test.DataTest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        DataTest.run();
+
+
+
+    }
+}
---
+++ src/main/java/cz/jpcz/cryptomanager/test/DataTest.java
@@ -0,0 +1,7 @@
+package cz.jpcz.cryptomanager.test;
+
+public class DataTest {
+    public static void run() {
+
+    }
+}
