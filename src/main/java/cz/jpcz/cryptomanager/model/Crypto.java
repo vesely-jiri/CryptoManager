38079925dac7bef3d043c2ebf6a755c94a5e4f04
vesely-jiri <vesely.jiri1998@gmail.com>
2025-06-05 14:54:10 +0200

    refactor: add Lombok annotations for getter, setter and constructor

--- src/main/java/cz/jpcz/cryptomanager/model/Crypto.java
+++ src/main/java/cz/jpcz/cryptomanager/model/Crypto.java
@@ -3,9 +3,17 @@ package cz.jpcz.cryptomanager.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 public class Crypto implements Comparable<Crypto> {
     @Id
@@ -15,56 +23,6 @@ public class Crypto implements Comparable<Crypto> {
     private String symbol;
     private Double price;
     private Double quantity;
-
-    public Crypto(Integer id, String name, String symbol, Double price, Double quantity) {
-        this.id = id;
-        this.name = name;
-        this.symbol = symbol;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getSymbol() {
-        return symbol;
-    }
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-    public Double getQuantity() {
-        return quantity;
-    }
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public String toString() {
-        return "Crypto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", symbol='" + symbol + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                '}';
-    }
 
     @Override
     public int compareTo(Crypto crypto) {
